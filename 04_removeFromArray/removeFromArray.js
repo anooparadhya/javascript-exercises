71470b1047286910e4f4f50a7cbd25afebe76972
anooparadhya <96173293+anooparadhya@users.noreply.github.com>
2025-03-17 20:18:32 +0530

    Update function logic

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(paramarray, ...deletionarray) {
+
+    let array = [];
+    for(let i = 0; i < paramarray.length; i++)
+    {
+        if(!deletionarray.includes(paramarray[i]))
+        {
+            array.push(paramarray[i]);
+        }
+    }
+    return array;
+
 };
 
 // Do not edit below this line
