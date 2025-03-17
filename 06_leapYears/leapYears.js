07cac20cc1d8b3b5b81d460907c02fbfbfad9f7d
anooparadhya <96173293+anooparadhya@users.noreply.github.com>
2025-03-17 20:47:38 +0530

    Update leapYears() logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(checkYear) {
+
+    if((checkYear % 4 === 0) && (checkYear % 100) != 0)
+    {
+        return true;
+    }
+    if((checkYear % 100 === 0) && (checkYear % 400 === 0))
+    {
+        return true;
+    }
+
+    return false;
 
 };
 
