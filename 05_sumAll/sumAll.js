934c19bdb709644620e260725a05b1ac56970d30
anooparadhya <96173293+anooparadhya@users.noreply.github.com>
2025-03-17 20:38:36 +0530

    Update SumAll() logic

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+
+    if(!(Number.isInteger(firstNumber) && Number.isInteger(secondNumber)))
+    {
+        return "ERROR";
+    }
+    if(firstNumber < 0 || secondNumber < 0)
+    {
+        return "ERROR";
+    }
+    let temp;
+    if(firstNumber > secondNumber)
+    {
+        temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+    }
+    let sum = 0;
+    for(let i = firstNumber; i <= secondNumber; i++)
+    {
+        sum += i;
+    }
+    return sum;
 
 };
 
