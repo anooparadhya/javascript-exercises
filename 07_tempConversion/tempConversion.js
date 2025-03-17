591d16cc7ccd91faac12f2a2057ef1675d10884d
anooparadhya <96173293+anooparadhya@users.noreply.github.com>
2025-03-17 20:57:53 +0530

    Update tempConversion() logic

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(toCelsius) {
+
+  toCelsius = (toCelsius - 32)*(5/9);
+  return +toCelsius.toFixed(1);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(toFahrenheit) {
+
+  toFahrenheit = ((toFahrenheit)*(9/5))+32;
+  return +toFahrenheit.toFixed(1);
+
 };
 
 // Do not edit below this line
